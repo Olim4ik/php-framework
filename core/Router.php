f763e1c4b2172122a70f12d20817a402cd09650b
Olim4ik <64145001+Olim4ik@users.noreply.github.com>
2022-05-27 16:29:30 +0500

    working with request and response || adding layouts and rendering view

--- core/Router.php
+++ core/Router.php
@@ -8,12 +8,19 @@ namespace app\core;
  */
 class Router
 {
+	public Request $request;
+	public Response $response;
 	protected array $routes = [];
-	protected Request $request;
 
-	public function __construct(Request $request)
+
+	/**
+	 * @param Request $request
+	 * @param Response $response
+	 */
+	public function __construct(Request $request, Response $response)
 	{
 		$this->request = $request;
+		$this->response = $response;
 	}
 
 	public function get($path, $callback)
@@ -27,12 +34,36 @@ class Router
 		$method = $this->request->getMethod();
 		$callback = $this->routes[$method][$path] ?? false;
 		if ($callback === false) {
-			echo "Not found";
-			exit();
+			$this->response->setStatusCode(404);
+			return "Not found";
 		}
-		echo call_user_func($callback);
+		if (is_string($callback)) {
+			return $this->renderView($callback);
+		}
+		return call_user_func($callback);
 
+	}
 
+	public function renderView($view)
+	{
+		$layoutContent = $this->layoutContent();
+		$viewContent = $this->renderOnlyView($view);
+		return str_replace('{{content}}', $viewContent, $layoutContent);
+//		include_once Application::$ROOT_DIR . '/views/' . $view . '.php';
+	}
+
+	protected function layoutContent()
+	{
+		ob_start();
+		include_once Application::$ROOT_DIR . '/views/layouts/main.php';
+		return ob_get_clean();
+	}
+
+	protected function renderOnlyView($view)
+	{
+		ob_start();
+		include_once Application::$ROOT_DIR . "/views/$view.php";
+		return ob_get_clean();
 	}
 
 }
